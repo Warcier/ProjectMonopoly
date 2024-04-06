15bf8ce3cab6ccbac3fe0a878da6edbb18641034
Rex124124 <103821532+Rex124124@users.noreply.github.com>
2024-04-07 04:12:38 +0800

    Add files via upload

---
+++ src/model/Dice.java
@@ -0,0 +1,16 @@
+package com3101_project;
+import java.util.Random;
+/**
+ *
+ * @author rexhe
+ */
+public class Dice {
+    private int Dice_Number;
+
+    public int getDiceNumber() {
+        // get random Dice Number 1 to 10 
+        Random rand = new Random();
+        Dice_Number = rand.nextInt(10) + 1;
+        return Dice_Number;
+    }
+}
