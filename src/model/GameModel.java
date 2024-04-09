cd94a47af3850a4125d67d19bd6fa240a288ad00
Susan Grg <56074470+Warcier@users.noreply.github.com>
2024-04-09 18:19:05 +0800

    fixed model

--- src/model/GameModel.java
+++ src/model/GameModel.java
@@ -1,5 +1,42 @@
 package model;
 
+import controller.GameController;
+import model.list.CircularLinkedList;
+
 public class GameModel {
-     
+    private Dice dice;
+    private GameController gameController;
+    private TurnController turnController;
+    private final CircularLinkedList board;
+
+
+    public GameModel() {
+        this.board = new CircularLinkedList(test.initPlayer(), test.initProperty());
+        this.turnController = new TurnController(test.initPlayer());
+    }
+
+    public CircularLinkedList getBoard() {
+        return board;
+    }
+
+    public void setGameController(GameController gameController) {
+        this.gameController = gameController;
+    }
+
+    public void moveCurrentPlayer() {
+        Player currentPlayer = turnController.getCurrentPlayer();
+        board.movePlayerToNextNode(currentPlayer, dice.getDiceNumber());
+    }
+
+    public void rollDice() {
+        dice.roll();
+    }
+
+
+    // TURN LOGIC
+    public void endTurn() {
+        turnController.nextTurn();
+        // Other end turn logic...
+    }
+
 }
--- src/model/Player.java
+++ src/model/Player.java
@@ -3,15 +3,33 @@ package model;
 import java.util.List;
 
 public class Player {
-    private String Name;
+    private String name;
+    private boolean isBankrupt;
+    private int cash;
     private List<Property> ownProperty;
 
     public Player(String name){
-        this.Name = name;
+        this.name = name;
     }
 
     public String getName(){
-        return Name;
+        return name;
+    }
+
+    public int getCash(){
+        return cash;
+    }
+
+    public boolean isBankrupt(){
+        return isBankrupt;
+    }
+
+    public void playerBankrupted() {
+        this.isBankrupt = true;
+    }
+
+    public void setCash(int amount){
+        this.cash = this.cash + amount;
     }
 
     public List<Property> getPlayerProperty(){
